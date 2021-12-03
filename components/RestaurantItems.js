db53d2155b7fdf91b8ac415829ad83a14cb4ed5e
Chau Cottrell <chauchac98@gmail.com>
2021-12-02 21:07:38 -0800

    Yelp API and Google places pulled

--- components/RestaurantItems.js
+++ components/RestaurantItems.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-const localRestaurants = [
+export const localRestaurants = [
     {
     name : "Beachside Bar",
     image_url:
@@ -33,10 +33,10 @@ const localRestaurants = [
   },
 ];
 
-export default function RestaurantItems() {
+export default function RestaurantItems(props) {
     return (
         <TouchableOpacity activeOpacity={1} style={{ marginBottom: 30 }}>
-            {localRestaurants.map((restaurant, index) => (
+            {props.restaurantData.map((restaurant, index) => (
             <View 
                 key={index}
                 style={{marginTop:10, padding: 15, backgroundColor: "white"}}
--- screens/Home.js
+++ screens/Home.js
@@ -1,21 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import Categories from '../components/Categories';
 import HeaderTabs from "../components/HeaderTabs"
-import RestaurantItems from '../components/RestaurantItems';
+import RestaurantItems, { localRestaurants } from '../components/RestaurantItems';
 import SearchBar from '../components/SearchBar';
 
+const YELP_API_KEY = "6vbA2ruVv0LvSfqcmkKrdrb6Mxd3T-TuQg0-30aGtkDWafuHZahuO4LS6Yt2KbvkGqXJzExYOp-RkqXmgSYFHR7u-xBA44nHVD-tBttdhQ07pd4wmfe2GFZZB5ipYXYx";
+
 export default function Home() {
-    return (
-        <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
-            <View style={{ backgroundColor: "white", padding: 15 }}>
-                <HeaderTabs/>
-                <SearchBar />
-            </View>
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <Categories />
-                <RestaurantItems />
-            </ScrollView>
-        </SafeAreaView>
-    );
-}
+    const [restaurantData, setRestaurantData] = useState(localRestaurants);
+    const [city, setCity] = useState('LosAngeles');
+
+    const getRestaurantFromYelp = () => {
+        const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
+
+    const apiOptions = {
+        headers: {
+            Authorization: `Bearer ${YELP_API_KEY}`,
+        },
+    };
+
+    return fetch(yelpUrl, apiOptions)
+        .then((res) => res.json())
+        .then(json =>
+            setRestaurantData(
+                json.businesses
+            ));
+    };
+
+    useEffect(() => {
+        getRestaurantFromYelp();
+    }, [city]);
+
+return (
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+        <View style={{ backgroundColor: "white", padding: 15 }}>
+            <HeaderTabs />
+            <SearchBar cityHandler={setCity} />
+        </View>
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <Categories />
+            <RestaurantItems 
+                restaurantData={restaurantData} 
+            />
+        </ScrollView>
+    </SafeAreaView>
+);
+};
